5b9a4970785a4cf28627f8f3e9cd7e2df12a136c
Julien CHATRY <chatryjulien@gmail.com>
2025-04-06 17:57:26 +0200

    Removed merge artifacts

--- Shaders/fragmentShader.fs
+++ Shaders/fragmentShader.fs
@@ -2,19 +2,12 @@
 out vec4 FragColor;
 
 in vec2 texCoord;
-<<<<<<< HEAD
-=======
 in vec4 outColor;
->>>>>>> c6f989f (Encapsulated camera related methods in a class + added ImGui to add a menu)
 
 uniform sampler2D myTexture;
 
 void main()
 {
    // FragColor = texture(myTexture, texCoord);
-<<<<<<< HEAD
-   FragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
-=======
    FragColor = outColor;
->>>>>>> c6f989f (Encapsulated camera related methods in a class + added ImGui to add a menu)
 };
